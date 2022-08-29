85d2b520b2b528f52f371e85e89bc129b9680782
Stefan020 <61162163+Stefan020@users.noreply.github.com>
2022-08-29 19:04:45 +0200

    Update top-match-worker.js
    addition

--- workers/top-match-worker.js
+++ workers/top-match-worker.js
@@ -1,70 +1,140 @@
-// const schedule = require('node-schedule');
-// const getRule = new schedule.RecurrenceRule();
-// const setRule = new schedule.RecurrenceRule();
-// const getTopData = require('../fetchData/topData').getTopData;
-// const setWinnings = require('../fetchData/setWinings').setWinings;
-// const saveMatchesStatistics = require('../fetchData/setWinings').saveMatchesStatistics;
-
-
-// getRule.hour = 9;
-// getRule.minute = 0;
-// getRule.tz = 'Etc/UTC';
-
-// const gettingJob = schedule.scheduleJob(getRule, () => {
-//     getTopData();
-// });
-
-// setRule.hour = 8;
-// setRule.minute = 30;
-// setRule.tz = 'Etc/UTC';
-
-// const settingJob = schedule.scheduleJob(setRule, async () => {
-//     await saveMatchesStatistics();
-//     setWinnings();
-// });
- 
 const fetch = require('node-fetch');
+const {  getMatchWinner} = require('./utils');
+const {getOneTicket, update} = require('../model/Ticket');
+const { getOne, updateUser } = require('../model/User');
 
-const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?date=2022-08-09'
+
+const today = new Date();
+const day = String(today.getDate()).padStart(2, '0');
+const month = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+const year = today.getFullYear();
+
+const url = `https://api-football-v1.p.rapidapi.com/v3/fixtures?date=${year}-${month}-${day}`
 
 const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '8935d9e334msh195ae00a70c5613p1e2cefjsnbfaf938a6951',
-      'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-    }
-  };
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '8935d9e334msh195ae00a70c5613p1e2cefjsnbfaf938a6951',
+    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+  }
+};
 
-  const getAdminGames = async(req,res) => {
-    let games = []
-    await fet
-    ch(url, options)
-	.then(res => res.json())
-	.then(json => 
-        
-          json.response.forEach(element => {
-            
-                let fixture = element.fixture
-                let teams = element.teams
-                
-                let game = {
-                    fid: fixture.id,
-                    time: fixture.date,
-                    home: teams.home.name,
-                    hlogo: teams.home.logo,
-                    away: teams.away.name,
-                    alogo: teams.away.logo,   
-                    score: element.goals 
-                }
-                
-                games.push(game)
-                // console.log(games)
-                
-            }))
-            .catch(err => console.error('error:' + err));
-            return games;
+/**
+ * @param {object} req 
+ * @param {object} res 
+ * @returns all games from rest api
+ */
+const getAdminGames = async (req, res) => {
+  let games = []
+  await fetch(url, options)
+    .then(res => res.json())
+    .then(json =>
+
+      json.response.forEach(element => {
+
+        let fixture = element.fixture
+        let teams = element.teams
+
+        let game = {
+          fid: fixture.id,
+          time: fixture.date,
+          home: teams.home.name,
+          hlogo: teams.home.logo,
+          away: teams.away.name,
+          alogo: teams.away.logo,
+          score: element.goals,
+          matchStatus: fixture.status?.short
+        }
+
+        games.push(game)
+
+      }))
+    .catch(err => console.error('error:' + err));
+  return games;
+}
+
+const checkTicket = async (ticket) => {
+ 
+  let game = {}
+  let gamesToCheck = []
+  let gamesAfterCheck = []
+  let chechedTicket
+  let ticketScore = {
+    played: 0,
+    correct: 0
+  }
+
+  let data = ticket.games
+
+  await data.forEach(el => {
+    if (el.matchStatus === 'FT') {
+        game = {
+        fid: el.fid,
+        hometeam: el.hometeam,
+        awayteam: el.awayteam,
+        finalScore: el.finalScore,
+        scoreComp: getMatchWinner(el.finalScore.home, el.finalScore.home),
+        bet: el.bet,
+        matchStatus: el.matchStatus,
+        betStatus: el.betStatus
+      }
+
+      gamesToCheck.push(game)
+    }
+
+  })
+  if (gamesToCheck.length !== 9) {
+    return
+  }else {
+    await gamesToCheck.forEach(async(el) => {
+      ticketScore.played + 1
+      if (el.scoreComp === el.bet) {
+        ticketScore.correct + 1
+        game = {
+          fid: el.fid,
+          hometeam: el.hometeam,
+          awayteam: el.awayteam,
+          finalScore: el.finalScore,
+          scoreComp: getMatchWinner(el.finalScore.home, el.finalScore.home),
+          bet: el.bet,
+          betStatus: 'green'
+        }
+        gamesAfterCheck.push(game)
+      } else {
+        game = {
+          fid: el.fid,
+          hometeam: el.hometeam,
+          awayteam: el.awayteam,
+          finalScore: el.finalScore,
+          scoreComp: getMatchWinner(el.finalScore.home, el.finalScore.home),
+          bet: el.bet,
+          betStatus: 'red'
+      }
+      gamesAfterCheck.push(game)
+    }
+  
+    let ticketToDB = {
+      games: gamesAfterCheck,
+      uid: ticket.uid,
+      isWinner: ticketScore.correct === 9,
+      date: ticket.date,
+      isTicketChecked: true
+    }
+  
+    let userToUpdate = updateUser({_id: ticket.uid}, {
+      betCount: betCount + ticketScore.played,
+      correctBetCount: correctBetCount + ticketScore.correct
+  })
+  
+    if (ticketScore.played === 9) {
+      chechedTicket = await update(ticket._id, ticketToDB)
+      }
+    })
+  }
 }
 
 module.exports = {
-    getAdminGames
+  getAdminGames,
+  checkTicket
 }
+ 
